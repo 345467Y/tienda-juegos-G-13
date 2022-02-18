5aa14758026284ccd09e45843b4131e610e6e362
Daniel3132 <danielcorrea3131@gmail.com>
2022-02-18 10:15:42 -0500

    formularios y header

---
+++ scripts/formulario.js
@@ -0,0 +1,7 @@
+js
+    function abrir(){
+      document.getElementById("opc").style.display="block";
+    }
+    function cerrar(){
+      document.getElementById("opc").style.display="none";
+    }
--- scripts/infoJuegos.js
+++ scripts/infoJuegos.js
@@ -1,15 +1,18 @@
 const elegido = JSON.parse(localStorage.getItem('video'));
 console.log(elegido);
 const contenedor = document.getElementById('allInfo')
+
 window.addEventListener('DOMContentLoaded', mostrarinfo());
 async function mostrarinfo(){
     
     elegido.forEach(element => {
         const{id, title, thumbnail, short_description, genre}=element
+        const titulo = document.getElementById('tituloTab')
+        titulo.innerHTML=title;
         contenedor.innerHTML=`
-        <h1 class="titulo" id="titulo"> ${title}</h1>
+            <h1 class="titulo" id="titulo"> ${title}</h1>
             <div id="imagenCont" class="imagenCont">
-            <img class="imagen" src="${thumbnail}" >
+           <img class="imagen" src="${thumbnail}" >
             <p class="genero">${genre}</p>
             <p class="info" id="info">
                 ${short_description}
